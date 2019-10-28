eaa6ab9f9ad98821886a6dffb9936040a7e35b1e
GDS2005 <42686303+GDS2005@users.noreply.github.com>
2019-10-28 18:11:31 -0300

    Add files via upload
    We manually upload OrderRepository and create method CreateTableIfNoExist

--- backend-java-app/src/dal/MySQLAccess.java
+++ backend-java-app/src/dal/MySQLAccess.java
@@ -2,15 +2,18 @@ package dal;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Connection;
 
 public class MySQLAccess {
+	
 	private Connection cnn = null;
 	   public Connection getConnection(){
 	      if (cnn == null) {
 	    	  try{
 		            cnn = DriverManager.getConnection("mysql://b0a9853bb934be:b5cc646e@us-cdbr-iron-east-05.cleardb.net/heroku_3f26ca8502dfdfc?reconnect=true", "b0a9853bb934be", "b5cc646e");
 		            System.out.println("Connected...");
+		            createTable();
 	            }catch (SQLException ex) {
 	        	 System.out.print("ERROR ENCONTRADO: "+ex);
 	        	 }
@@ -22,4 +25,31 @@ public class MySQLAccess {
 	         cnn.close();
 	      }
 	   }
+	   
+	   private void createTable() throws SQLException {
+		    String sqlCreate1 = "CREATE TABLE IF NOT EXISTS articles "
+		            + "  (id_article           INTEGER,"
+		            + "   code_article            INTEGER,"
+		            + "   name_article          VARCHAR(10),"
+		            + "   stock_article           INTEGER,"
+		            + "   unit_prince_article           INTEGER)";
+		    
+		    String sqlCreate2 = "CREATE TABLE IF NOT EXISTS orders "
+		            + "  (id_order           INTEGER,"
+		            + "   number_order            INTEGER,"
+		            + "   username          VARCHAR(10),"
+		            + "   name_client           VARCHAR(10),"
+		            + "   total_order           INTEGER)";
+		    
+		    String sqlCreate3 = "CREATE TABLE IF NOT EXISTS detail_order "
+		            + "  (number_order           INTEGER,"
+		            + "   code_article            INTEGER,"
+		            + "   quantity_article          INTEGER,"
+		            + "   unit_prince_article           INTEGER)";
+
+		    Statement stmt = cnn.createStatement();
+		    stmt.execute(sqlCreate1);
+		    stmt.execute(sqlCreate2);
+		    stmt.execute(sqlCreate3);
+		}
 }
---
+++ backend-java-app/src/dal/OrderRepository.java
@@ -0,0 +1,36 @@
+package dal;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import entities.Order;
+import java.util.ArrayList;
+
+public class OrderRepository {
+	private final String table = "orders";
+/*We pull all the table order*/ 
+	   public ArrayList<Order> getOrder(Connection cnx) throws SQLException {
+	      ArrayList<Order> ordersList = new ArrayList<Order>();
+	      try{
+	         PreparedStatement query = cnx.prepareStatement("SELECT * FROM " + this.table);
+	         ResultSet result = query.executeQuery();
+	         while(result.next()){
+	        	 
+	             int id_order = Integer.parseInt(result.getString("id_order")) ;
+	             int number_order = Integer.parseInt(result.getString("number_order"));
+	             String username = result.getString("username");
+	             int date_order = Integer.parseInt(result.getString("date_order"));
+	             String name_client = result.getString("name_client");
+	             int total_order = Integer.parseInt(result.getString("total_order"));
+
+	            Order o = new Order(id_order,number_order,date_order,username,name_client,total_order); //Create the object article
+	            ordersList.add(o); //Add the object 
+	            System.out.println("SELECT Realizado - Objeto producto realizado de forma exitosa.");
+	         }
+	      }catch(SQLException ex){
+	         throw new SQLException(ex);
+	      }
+	      return ordersList;
+	   }       
+}
